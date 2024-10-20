2850e42efd6e6deebee62db30b735d4fbfc1c841
mori-dv <morimori.ph@gmail.com>
2024-10-20 19:35:40 +0330

    rename a column name of the book table

--- core/models.py
+++ core/models.py
@@ -9,11 +9,11 @@ class Category(models.Model):
 
 
 class Book(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='books')
     description = models.TextField(null=True, blank=True)
     published_date = models.DateField(auto_now_add=True)
     author = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.title
