626eba9be77c7d18f0118b7636aaa28b635f8466
mori-dv <morimori.ph@gmail.com>
2024-10-20 19:29:18 +0330

    remove unique parameter from foreignkey field which worked as one2one field

--- core/models.py
+++ core/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
 class Book(models.Model):
     name = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, unique=True, related_name='books')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='books')
     description = models.TextField(null=True, blank=True)
     published_date = models.DateField(auto_now_add=True)
     author = models.CharField(max_length=100)
